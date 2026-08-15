66861d894ecb8b47694bed526a14be07c90b3eab
Dean Ward <dean@oveio.io>
2026-08-15 20:13:53 +0100

    Remove Vue main.js from astro branch

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router.js'
-import { boot } from './store.js'
-import './style.css'
-
-boot()
-createApp(App).use(router).mount('#app')

b6620c96efaa20c666fa0e04e0c09b0cedd0cd71
Dean Ward <dean@oveio.io>
2026-08-15 20:23:19 +0100

    Retire leftover Vue sources in place

--- src/main.js
+++ src/main.js
@@ -1,8 +1,1 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router.js'
-import { boot } from './store.js'
-import './style.css'
-
-boot()
-createApp(App).use(router).mount('#app')
+// Retired. This repo is an Astro static site now.
